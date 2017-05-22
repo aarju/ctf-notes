3cd37efbd473d0a2538a4df8b63623a350590fec
Aaron Jewitt <aarju@users.noreply.github.com>
2017-05-22 15:34:50 +0200

    Update hour.ps1

--- windows/PowerShell/hour.ps1
+++ windows/PowerShell/hour.ps1
@@ -1,9 +1,9 @@
 # Get a list of files created within the last hour
-Get-ChildItem -Recurse -ErrorAction SilentlyContinue -Filter *  | ? {$_.CreationTime -gt (get-date).AddHours(-1)}
+Get-ChildItem -Recurse -file -ErrorAction SilentlyContinue -Filter *  | ? {$_.CreationTime -gt (get-date).AddHours(-1)}
 
 # Get a list of files modified within the last hour
-Get-ChildItem -Recurse -ErrorAction SilentlyContinue -Filter *  | ? {$_.LastWriteTime -gt (get-date).AddHours(-1)}
+Get-ChildItem -Recurse -file -ErrorAction SilentlyContinue -Filter *  | ? {$_.LastWriteTime -gt (get-date).AddHours(-1)}
 
-Get-ChildItem -Recurse -ErrorAction SilentlyContinue -Filter *  | ? {$_.LastAccessTime -gt (get-date).AddHours(-1)}
+Get-ChildItem -Recurse -file -ErrorAction SilentlyContinue -Filter *  | ? {$_.LastAccessTime -gt (get-date).AddHours(-1)}
 
 
