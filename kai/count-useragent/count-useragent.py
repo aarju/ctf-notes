ab47fb36dd36ea4b89b5534d05cee8dc25204184
Aaron Jewitt <jewitta@gmail.com>
2017-11-04 12:59:00 +0100

    a

---
+++ kai/count-useragent/count-useragent.py
@@ -0,0 +1,11 @@
+'''
+You are checking in on the requests to your web server from within your network. 
+According to some documentation, everyone on the network should have and be using 
+Google Chrome version 59 or higher, Microsoft Edge version 12 or higher, or 
+Mozilla Firefox version 40 or higher. In the attached request log, how many 
+requests came from other things?
+'''
+
+
+
+import sys
