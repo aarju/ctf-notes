89b5b9a4bca51b055cd6cf83b36b812fc3080e15
aaron <jewitta@gmail.com>
2018-05-05 09:34:16 +0200

    Initial commit of evt2ls, dns_over_https, and invoke-send2logstash

---
+++ windows/Invoke-Send2LogStash.ps1
@@ -0,0 +1,7 @@
+# easy to deploy script that will collect event logs from a 
+# host and send them to a LogStash injestor for forensic analysis
+
+# one time options, or the ability to install it as a script that forwards
+# new events every 5 minutes
+
+functio
